70aaa6384f878679915416f490dde35c414e7a1d
hiki7 <rsaliyev03@gmail.com>
2024-08-01 00:05:38 +0500

    added endpoint for saving and fetching the anime list

--- src/main_app/main.py
+++ src/main_app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI, Request, Response, HTTPException
 from pydantic import BaseModel
 import requests
-from typing import List
-from models import AnimeItem
-from config.db_connect import engine
+from typing import List, Optional
+from .models import Anime
+from .config.db_connect import engine
+
+from sqlmodel import SQLModel, Session, create_engine, Field
 
 app = FastAPI()
 
-url = "https://kitsu.io/api/edge/anime"
-headers = {
+URL = "https://kitsu.io/api/edge/anime"
+HEADERS = {
     "Accept": "application/vnd.api+json",
     "Content-Type": "application/vnd.api+json"
 }
@@ -18,28 +20,57 @@ headers = {
 #     anime_id: str
 #     user_id: str
 
-class AnimeItemResponse(BaseModel):
-    id: str
-    title: str
-    synopsis: str
-    poster_image: str
+# class AnimeItemResponse(BaseModel):
+#     id: str
+#     title: str
+#     synopsis: str
+#     poster_image: str
 
 
 # Fetch the list of anime
-@app.get("/anime", response_model=List[AnimeItemResponse])
-def fetch_anime():
-    response = requests.get(url, headers=headers)
+# @app.get("/anime", response_model=List[AnimeItemResponse])
+# def fetch_anime():
+#     response = requests.get(url, headers=headers)
+#
+#     if response.status_code == 200:
+#         anime_list = response.json()['data']
+#         return [
+#             {
+#                 "id": anime["id"],
+#                 "title": anime["attributes"]["canonicalTitle"],
+#                 "synopsis": anime["attributes"]["synopsis"],
+#                 "poster_image": anime["attributes"]["posterImage"]["medium"],
+#             }
+#             for anime in anime_list
+#         ]
+#     else:
+#         raise HTTPException(status_code=response.status_code, detail="Failed to fetch anime list")
+
+
+@app.post("/save-anime")
+def save_anime():
+    response = requests.get(URL, headers=HEADERS)
 
     if response.status_code == 200:
         anime_list = response.json()['data']
-        return [
-            {
-                "id": anime["id"],
-                "title": anime["attributes"]["canonicalTitle"],
-                "synopsis": anime["attributes"]["synopsis"],
-                "poster_image": anime["attributes"]["posterImage"]["medium"],
-            }
+        anime_objects = [
+            Anime(
+                title=anime['attributes']['canonicalTitle'],
+                synopsis=anime['attributes']['synopsis'],
+                poster_image=anime['attributes']['posterImage']['medium']
+            )
             for anime in anime_list
         ]
+
+        with Session(engine) as session:
+            session.add_all(anime_objects)
+            session.commit()
+        return {"message": "Anime saved!"}
     else:
-        raise HTTPException(status_code=response.status_code, detail="Failed to fetch anime list")
+        raise HTTPException(status_code=response.status_code, detail="Failed to save anime!")
+
+@app.get("/anime", response_model=List[Anime])
+def get_anime():
+    with Session(engine) as session:
+        anime_list = session.query(Anime).all()
+        return anime_list
--- src/main_app/models.py
+++ src/main_app/models.py
@@ -2,9 +2,16 @@ from typing import Optional
 
 from sqlmodel import SQLModel, Field
 
-class Anime(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+
+class AnimeCreate(SQLModel):
     title: str
     synopsis: str
     poster_image: str
+
+
+class Anime(AnimeCreate, SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    # title: str
+    # synopsis: str
+    # poster_image: str
 
